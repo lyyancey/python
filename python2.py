090cd98bc004d93246a16f190474babff24e07ad
yancey <1449296476@qq.com>
2020-06-04 23:46:06 +0800

    函数结束

--- python2.py
+++ python2.py
@@ -131,12 +131,128 @@
 #
 #
 # 可变参数
-def calc(*numbers):
-    sum = 0
-    for n in numbers:
-        sum = sum + n*n
-    return sum
-
-
-print(calc(1, 2, 3))
-print(calc(1, 2))
+# def calc(*numbers):
+#     sum = 0
+#     for n in numbers:
+#         sum = sum + n*n
+#     return sum
+#
+#
+# print(calc(1, 2, 3))
+# print(calc(1, 2))
+# print(calc())
+# 定义可变长参数时，只需要在参数前面加一个*号，在函数内部，参数收到的是一个tuple，因此函数代码完全不变。
+# 在调用参数时可以传入任意个参数，包括0个参数。
+#
+#
+# 如果现在已经有一个list或tuple 但是函数参数是可变长参数怎么办，解决方法有两种：
+#   1.将list或tuple拆开传到函数里，calc(num[0], num[1], num[2])
+#   2.Python允许在list或tuple 前面加一个*号，把list或tuple的元素变成可变长参数传进去：calc(*num),num=[1,2,3]
+# 其中，*num 代表把num这个list的所有元素作为可变长参数传进去。
+#
+# ! 感叹号表示Alert
+# ? 问号表示Queries
+# TODOs:TODOs表示待办事项
+# * 表示高亮
+#
+#
+# !关键字参数，关键字参数可以扩展函数功能
+# 关键字参数允许你传入0个或任意个含参数名的参数，这些关键字参数内部自动组装为一个dict
+# def person(name, age, **kw):
+#     print('name', name, 'age', age, 'other', kw)
+#
+#
+# person('Michael', 30)
+# person('Bob', 35, city='Beijing')
+# person('Adam', 45, gender='M', job='Engineer')
+# * 下面**extra表示把extra这个dict的所有key-value用关键字参数传入到函数**kw参数，
+# * kw将获得一个dict，注意在kw这个dict只是extra的一个拷贝，对kw的改动不会影响到函数外的extra
+# extra={"set": 'qwer'}
+#  person('Yancey', 32, **extra)
+#
+#
+#
+#
+#
+# ! 命名关键字参数。
+# 对于关键字参数，函数的调用者可以传入任意不受限制的关键字参数。至于传的哪些参数，需要在函数内部通过kw检查，如：
+# def person(name, age, **kw):
+#     if 'city' in kw:
+#         # 有city参数
+#         pass
+#     if 'job' in kw:
+#         # 有job参数
+#         pass
+#     print('name', name, 'age', age, kw)
+#
+#
+# person('Jack', 24, city='Beijing', addr='Chaoyang', zipcode=123456)
+# !为了限制关键字的名字,可以用命名关键字参数，如只接收city和job作为关键字参数，这种方式定义如下：
+# ! def person(name, age, *,city , job):
+# !     print(name, age, city, job)
+# ! 和关键字参数**kw不同,命名关键字参数需要需要一个特殊分隔符 * , * 后面的参数被视为命名关键字参数。
+# ! 其调用方式如下：
+# ! person('Jack', 24, city='Beijing', job='Engineer')
+#
+#
+# !如果函数定义中已经有了一个可变参数,后面跟着命名关键字参数就不再需要一个特殊的分隔符*了。
+# def person(name, age, *args, city, job):
+#     print(name, age, args, city, job)
+#
+#
+# person('张三', 23, 123, 234, city='Beijing', job='hardware')
+#
+# 当命名关键字有默认值时，可以不传入该参数。
+# 命名关键字参数必须传入参数名，和位置参数不同，如果不传入参数名会报错。
+# ! 使用命名关键字时，特别注意，如果没有可变长参数，就必须加一个*作为分隔符,缺少*，Python解释器将无法识别位置参数和命名关键字参数
+#
+#
+#
+#
+# ! 在Python中定义函数，可以用必选参数、默认参数、可变参数、关键字参数和命名关键字参数，这五种参数都可以组合使用。
+# ! 但是参数的定义顺序必须是：必选参数、默认参数、可变参数、命名关键字参数和关键字参数
+# def f1(a, b, c=0, *args, **kw):
+#     print('a =', a, 'b = ', b, 'c = ', c, 'args = ', args, 'kw = ', kw)
+#
+#
+# def f2(a, b, c=0, *, d, **kw):
+#     print('a = ', a, 'b = ', b, 'c = ', c, 'd = ', d, 'kw = ', kw)
+#
+#
+# f1(1, 2)
+# !还可以通过一个tuple和dict，调用上述函数：
+# args = (1, 2, 3, 4)
+# kw = {'d': 99, 'x': '#'}
+# f1(*args, **kw)
+# !所以对于任意函数，都可以通过类似func(*args, **kw)的形式调用它，无论他的参数是如何定义的。
+# !虽然可以组合多大5种参数，但不要同时使用太多的组合，否则函数接口的可理解性很差！
+#
+#
+#
+# 递归函数
+# def fact(n):
+#     if n == 1:
+#         return 1
+#     return n*fact(n - 1)
+# print(fact(1))
+# print(fact(5))
+# print(fact(100))
+# print(fact(1000))  # !堆栈会溢出
+# !解决堆栈溢出的方法是通过尾递归优化，事实上尾递归和循环效果是一样的，所以，把循环看成一种特殊的递归也是可以的。
+# ! 尾递归是指，在函数返回的时候，调用自身本身，并且，return语句不能包含表达式。
+# ! 这样，编译器或者解释器就可以把尾递归做优化，使递归本身无论调用多少次，都只占用一个堆栈，不会出现栈溢出的情况。
+# 上面的函数做成尾递归的形式：
+#
+#
+# def fact(n):
+#     return fact_iter(n, 1)
+#
+#
+# def fact_iter(num, product):
+#     if num == 1:
+#         return product
+#     return fact_iter(num - 1, num * product)
+#
+#
+# print(fact(100))
+# ! 遗憾的是Python标准的解释器并没有针对尾递归做优化，任何递归函数都存在栈溢出的问题。
